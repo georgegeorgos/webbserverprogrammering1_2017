e49562f5ffbcc92edaa35473aa006be3b9238e92
Pascal Olivier <pascal.olivier@utb.salem.se>
2017-12-20 13:40:32 +0100

    Add files via upload

---
+++ html/uppgifter/kap10 - felhantering original.php
@@ -0,0 +1,14 @@
+﻿<?php 
+
+//---- kod för Testkörning -------- 
+header ("Content-type: text/html; charset=utf-8");
+
+
+//---- skapa falska fel ---------
+echo "<p>A<p>";
+trigger_error("Fel ett", E_USER_WARNING);
+echo "<p>B</p>"; 
+trigger_error("Fel två", E_USER_ERROR); 
+echo "<p>C</p>"; 
+trigger_error ("Fel tre", E_USER_NOTICE);
+	 

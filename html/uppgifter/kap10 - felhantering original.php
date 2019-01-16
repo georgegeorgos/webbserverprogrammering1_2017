30ca30998e320a94f7010abd4c75233c2c2cd889
Pascal Olivier <pascal.olivier@utb.salem.se>
2019-01-16 09:25:44 +0100

    Update kap10 - felhantering original.php

--- html/uppgifter/kap10 - felhantering original.php
+++ html/uppgifter/kap10 - felhantering original.php
@@ -7,8 +7,11 @@ header ("Content-type: text/html; charset=utf-8");
 //---- skapa falska fel ---------
 echo "<p>A<p>";
 trigger_error("Fel ett", E_USER_WARNING);
+//---- skapa falsk fel  ---------
 echo "<p>B</p>"; 
 trigger_error("Fel två", E_USER_ERROR); 
+//---- skapa falsk fel  ---------
 echo "<p>C</p>"; 
 trigger_error ("Fel tre", E_USER_NOTICE);
-	 
+//---- visa text (se om hela filen har körts) --------- 
+echo "<p>filen har körts tills slutet</p>"; 
